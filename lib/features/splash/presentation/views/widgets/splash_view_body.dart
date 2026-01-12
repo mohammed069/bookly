8aa04d7147852586477f1d8960875390ae5e1dac
mohammed069 <mohammeddddd069@gmail.com>
2026-01-12 11:04:19 +0200

    change theme && add text to splash scree ^_^

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -9,7 +9,11 @@ class SplashViewBody extends StatelessWidget {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [Image.asset(AssetsData.logo)],
+      spacing: 10,
+      children: [
+        Image.asset(AssetsData.logo),
+        const Text('Read Free Books', textAlign: TextAlign.center),
+      ],
     );
   }
 }
