40dce149377befaa8cf7a97a92a962f40a70b4c1
mohammed069 <mohammeddddd069@gmail.com>
2026-01-16 14:58:15 +0200

    add search Text Field to searchView ^_^

--- lib/features/home/presentation/views/widgets/custom_app_bar.dart
+++ lib/features/home/presentation/views/widgets/custom_app_bar.dart
@@ -1,6 +1,8 @@
+import 'package:bookly/core/utils/app_router.dart';
 import 'package:bookly/core/utils/assets_data.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomAppBar extends StatelessWidget {
   const CustomAppBar({super.key});
@@ -12,7 +14,9 @@ class CustomAppBar extends StatelessWidget {
         Image.asset(AssetsData.logo, height: 20),
         const Spacer(),
         IconButton(
-          onPressed: () {},
+          onPressed: () {
+            context.push(AppRouter.searchView);
+          },
           icon: Icon(
             FontAwesomeIcons.magnifyingGlass,
             size: 20,
--- lib/features/serach/presentation/views/widgets/search_view_body.dart
+++ lib/features/serach/presentation/views/widgets/search_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/features/serach/presentation/views/widgets/custom_search_text_field.dart';
 import 'package:flutter/material.dart';
 
 class SearchViewBody extends StatelessWidget {
@@ -5,6 +6,9 @@ class SearchViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: const Column(children: [CustomSearchTextField()]),
+    );
   }
 }
