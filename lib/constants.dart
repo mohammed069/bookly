46ea53347b2285b5398c2cd8a729d6552e991758
mohammed069 <mohammeddddd069@gmail.com>
2026-01-18 16:48:39 +0200

    registerAdaptor & openBox ^_^

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,3 +2,4 @@ import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0xff100B20);
 const kGtSectraFine = 'GT Sectra Fine Regular';
+const kFeaturedBox = 'featured_box';
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:bookly/constants.dart';
 import 'package:bookly/core/utils/app_router.dart';
+import 'package:bookly/features/home/domain/entities/book_entity.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive_flutter/adapters.dart';
 
-void main() {
+void main() async {
+  Hive.registerAdapter(BookEntityAdapter());
+  await Hive.openBox(kFeaturedBox);
   runApp(const MyApp());
 }
 
