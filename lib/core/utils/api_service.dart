9423d6cd417469d01c92943d773b18d2f290fac6
mohammed069 <mohammeddddd069@gmail.com>
2026-01-18 10:50:52 +0200

    create api service ^_^

---
+++ lib/core/utils/api_service.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:dio/dio.dart';
+
+class ApiService {
+  ApiService(this._dio);
+
+  final Dio _dio;
+  final baseUrl = 'https://www.googleapis.com/books/v1/';
+
+  Future<Map<String, dynamic>> get({required String endPoint}) async {
+    var response = await _dio.get('$baseUrl$endPoint');
+    return response.data;
+  }
+}
