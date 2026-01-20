e9b5f6448c5c4c9046663b4d994367f7abcf5490
mohammed069 <mohammeddddd069@gmail.com>
2026-01-20 11:10:41 +0200

    delete package bloc and add package flutter_bloc ^_^

--- lib/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
+++ lib/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
@@ -1,9 +1,9 @@
-import 'package:bloc/bloc.dart';
 import 'package:bookly/core/errors/failure.dart';
 import 'package:bookly/features/home/domain/entities/book_entity.dart';
 import 'package:bookly/features/home/domain/use_cases/fetch_featured_books_use_case.dart';
 import 'package:bookly/features/home/presentation/manager/featured_books_cubit/featured_books_state.dart';
 import 'package:dartz/dartz.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
   FeaturedBooksCubit(this.featuredBooksUseCase) : super(FeaturedBooksInitial());
--- lib/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart
+++ lib/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart
@@ -1,9 +1,9 @@
-import 'package:bloc/bloc.dart';
 import 'package:bookly/core/errors/failure.dart';
 import 'package:bookly/features/home/domain/entities/book_entity.dart';
 import 'package:bookly/features/home/domain/use_cases/fetch_news_books_use_case.dart';
 import 'package:bookly/features/home/presentation/manager/newest_books_cubit/newest_books_state.dart';
 import 'package:dartz/dartz.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class NewestBooksCubit extends Cubit<NewestBooksState> {
   NewestBooksCubit(this.fetchNewsBooksUseCase) : super(NewestBooksInitial());
