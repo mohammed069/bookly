c15fcc4e639db24c04d62d99e831bfeb333f5dc7
mohammed069 <mohammeddddd069@gmail.com>
2026-01-20 10:44:52 +0200

    create newestBooks Cubit ^_^

---
+++ lib/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart
@@ -0,0 +1,25 @@
+import 'package:bloc/bloc.dart';
+import 'package:bookly/core/errors/failure.dart';
+import 'package:bookly/features/home/domain/entities/book_entity.dart';
+import 'package:bookly/features/home/domain/use_cases/fetch_news_books_use_case.dart';
+import 'package:bookly/features/home/presentation/manager/newest_books_cubit/newest_books_state.dart';
+import 'package:dartz/dartz.dart';
+
+class NewestBooksCubit extends Cubit<NewestBooksState> {
+  NewestBooksCubit(this.fetchNewsBooksUseCase) : super(NewestBooksInitial());
+
+  FetchNewsBooksUseCase fetchNewsBooksUseCase;
+  Future<void> fetchNewestBooks() async {
+    Either<Failure, List<BookEntity>> result =
+        await fetchNewsBooksUseCase.call();
+
+    result.fold(
+      (failure) {
+        emit(NewestBooksFailure(errorMessage: failure.message));
+      },
+      (books) {
+        emit(NewestBooksSuccess(books: books));
+      },
+    );
+  }
+}
