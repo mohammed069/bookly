bec819d19df6df5a56b051f8d5a16ecb67566162
mohammed069 <mohammeddddd069@gmail.com>
2026-01-21 09:24:02 +0200

    refactor: update saveData && enhance JSON parsing in VolumeInfo model && add FeaturedBooksListViewBlocBuilder

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,7 +1,7 @@
 import 'package:bookly/core/utils/styles.dart';
 import 'package:bookly/features/home/presentation/views/widgets/book_list_view_item.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_app_bar.dart';
-import 'package:bookly/features/home/presentation/views/widgets/featured_books_list_view.dart';
+import 'package:bookly/features/home/presentation/views/widgets/featured_books_list_view_bloc_builder.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -19,7 +19,7 @@ class HomeViewBody extends StatelessWidget {
               spacing: 20,
               children: [
                 CustomAppBar(),
-                FeaturedBooksListView(),
+                FeaturedBooksListViewBlocBuilder(),
                 Text("Best Seller", style: Styles.textStyle18),
               ],
             ),
--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,7 @@ class MyApp extends StatelessWidget {
           create: (context) {
             return FeaturedBooksCubit(
               FetchFeaturedBooksUseCase(getIt.get<HomeRepoImpl>()),
-            );
+            )..fetchFeaturedBooks();
           },
         ),
         BlocProvider(
